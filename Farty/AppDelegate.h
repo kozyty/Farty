8b7f4647fabd5026eab508cf7979c076f4095571
kozyty <kozyty@gmail.com>
2014-02-05 13:47:52 +0900

    Initial Commit

---
+++ Farty/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Farty
+//
+//  Created by kozyty on 2014/02/05.
+//  Copyright (c) 2014年 kozyty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Farty/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Farty
+//
+//  Created by kozyty on 2014/02/05.
+//  Copyright (c) 2014年 kozyty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
